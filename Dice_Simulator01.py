30778cd6cb060ad40ae71d18708912aa402dfc27
Lobsang Shakya <lobsangshakya5@gmail.com>
2023-08-11 11:37:12 +0530

    Basics Added

--- Dice_Simulator01.py
+++ Dice_Simulator01.py
@@ -1,22 +1,23 @@
-import random
+import random # This generates random numbers from given lists :
 
-def dice_range():
+def dice_range(): # Genrating of the number between 1-6 : 
     return random.randint(1, 6)
 
-def main():
+def main(): # This is the main function :
     print("Welcome to the Dice Simulator || Roll the dice and enjoy")
     
-    while True:
-        input("Press any key to roll the dice : ")
-        result = dice_range()
-        print("You rolled : ", result)
+    while True: 
+        input("Press any key to roll the dice : ") # This is the input section where you press any key and the number will be generated :
+        result = dice_range() # Calls the function from dice_range to generate numbers : 
+        print("You rolled : ", result) # Printing of the results : 
         
-        play_again = input("Do you want to roll again? (yes/no): ")
-        if play_again.lower() != "yes":
+        play_continue = input("Do you want to roll again? (yes/no): ") # Here, it tells whethere you want to continue to roll or not :
+        if play_continue.lower() != "yes": # If the user enters "yes", It continues the game : 
             print("Thank you for using the Dice Simulator!")
+            print("Hoping to see you again!")
             break
         else:
             print("Let's roll again!")
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
